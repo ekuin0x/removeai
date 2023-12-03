553265d0e0aad164d2d6dec5257c70972b2e5b04
ekuin0x <salhimouiz1@gmaiL.com>
2023-12-03 20:37:52 +0100

    Final

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
 from flask import Flask, flash, request, redirect, url_for, render_template
 from rembg import remove
-import os
-import random
-import pyrebase
-from dotenv import load_dotenv
-
-load_dotenv()
-firebase_api_key = os.getenv("FIREBASE_API_KEY")
-print(firebase_api_key)
-config = {
-  "apiKey": firebase_api_key,
-  "authDomain": "flask-firebase-storage.firebaseapp.com",
-  "databaseURL": "",
-  "storageBucket": "flask-firebase-storage.appspot.com",
-  "appId": "1:670694688551:web:d627e473ec0bce9112cb02",
-  "measurementId": "G-DSWWEJCHDT"
-}
-firebase = pyrebase.initialize_app(config)
-storage = firebase.storage()
+from fire import storage
 
 app = Flask(__name__)
 
@@ -25,17 +8,17 @@ app = Flask(__name__)
 def index() :
     return render_template("index.html")
 
-@app.route('/upload', methods = ['GET', 'POST'])
-def upload():
+@app.route('/data', methods = ['POST'])
+def data():
     if request.method == 'POST': 
-        img = request.files['image']
-        oname = os.path.join("static/results/", img.filename)
-        input = img.read()
-        output = remove(input)
-        imgurl = storage.child("static/results/" + img.filename).put(output)
-        url = storage.child("static/results/" + img.filename).get_url(imgurl['downloadTokens'])
-        return url
-    return 'Wrong Request'
+        img = request.files['file']
+        name = "(no_background)" + img.filename
+        i = remove(img.read())
+        storage.child(name).put(i)
+        url = storage.child(name).get_url("")
+        return url     
+    
+    return "PROBLEM OCCURED"
 
 if __name__ == '__main__' :
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,33 +1,15 @@
-var upload = function(){
-    var image = $('#input')[0].files[0];
-    formdata = new FormData();
-    formdata.append( 'image', image );
-    formdata.append( 'action', 'save-image' );
-
-    $(".flex1").html("<img id='wait' src='static/media/giphy.gif' alt='output' /><p<Please Wait</p>")
-
-    
-
-    $.ajax({
-        url: '/upload',
-        type: 'POST',
-        data: formdata,
-        processData: false,
-        contentType: false,
-        success: function( res ) {
-            console.log(res)
-            $(".flex1").html(
-    
-                "<a href='" + res +  "' download ><img id='output' src='" + res + "' alt='output'></a>"+
-                "<p>Click On Image To Download</p>"+
-                "<div class='button' id='reupload'>upload" +
-                "<input type='file' id='input' class='input' name='image' accept='Image/*' onchange='upload()'> "+
-                "</div>"
-            )
-        },
-        error : function(err) {
-            console.log("Erorrrr")
-        }
-    });
-}
-
+$("input").change(async function(){
+    let file = $("input").prop("files")[0]
+    $("#drop").html("<img src='/static/media/giphy.gif' />")
+    $("#drop").css("background-color", "transparent")
+    var data = new FormData()
+    data.append('file', file)
+    let res = await fetch("/data", {
+        method : 'post',
+        body : data
+    })
+    url = await res.text()
+    $("#drop").html(`<img src='${url}' />`)
+    $(".button").css("display", "block")
+    $("#save").attr("href", url)    
+})
